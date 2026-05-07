d7e53306418041979b7da083cab74fc21da0ddde
zzzealed <zzzealed@users.noreply.github.com>
2026-05-07 10:47:07 +0000

    deploy: d8037c9357cfe0b274db1d23b6c37e32c6cdf5d5

--- mute-exclude-channel/plugin.js
+++ mute-exclude-channel/plugin.js
@@ -67,7 +67,7 @@ function removeChannel(guildId, channelId) {
 	updateChannels(data);
 }
 function onLoad() {
-	store.exemptedChannels = {};
+	store.exemptedChannels ??= {};
 	const data = { ...store.exemptedChannels };
 	setChannels(data);
 	for (const ids of Object.values(data)) ids.forEach((id) => exemptedSet.add(id));
